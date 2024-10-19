a7db7d7c697968af245fe38d3432a102d297d9bf
Sahil Sinha <dev.sahilsinha@gmail.com>
2024-10-19 10:37:30 -0700

    Dual prompt to VScode editor command

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ load_dotenv()
 groq_key = os.environ.get("GROQ_API_KEY")
 
 SAMPLE_RATE = 44100
-DURATION = 10
+DURATION = 5
 
 
 def record_audio(filename: str, duration: int, sample_rate: int):
@@ -25,7 +25,7 @@ def record_audio(filename: str, duration: int, sample_rate: int):
         wf.writeframes(audio_data.tobytes())
 
 
-def transcribe_audio(filename):
+def get_command(filename):
     client = Groq()
     
     filename = os.path.dirname(__file__) + "/Audio/output_audio.wav"
@@ -34,7 +34,6 @@ def transcribe_audio(filename):
         transcription = client.audio.transcriptions.create(
         file=(filename, file.read()),
         model="distil-whisper-large-v3-en", 
-        prompt="Specify context or spelling", 
         response_format="json", 
         language="en", 
         temperature=0.0  
@@ -42,12 +41,33 @@ def transcribe_audio(filename):
     )
     
     print(transcription.text)
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are an assistant that helps generate VSCODE editor actions from VSCode api from user instructions. Only reply with the editor commands."
+            },
+            {
+                "role": "user",
+                "content": f"Convert this speech into a VSCODE command: '{transcription.text}'"
+            }
+        ],
+        model="llama3-8b-8192",
+        temperature=0.5,
+        max_tokens=1024,
+        top_p=1,
+        stop=None,
+        stream=False,
+    )
+    
+    vscode_command = chat_completion.choices[0].message.content
+    print("VSCODE Command:", vscode_command)
 
 
 def main():
     filename = os.path.dirname(__file__) + "/Audio/output_audio.wav"
     record_audio(filename, DURATION, SAMPLE_RATE)
-    transcribe_audio(filename)
+    get_command(filename)
 
 if __name__ == "__main__":
     main()
