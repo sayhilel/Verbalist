f5088539046b6a0df7f0788ad8a43b763772186c
Sahil Sinha <dev.sahilsinha@gmail.com>
2024-10-19 11:20:39 -0700

    Merged lucien's Audiorecorder

--- Voice/groqCommands.py
+++ Voice/groqCommands.py
@@ -1,31 +1,12 @@
 import os
-import wave
-import sounddevice as sd
 from dotenv import load_dotenv
 from groq import Groq
+
 
 load_dotenv()
 groq_key = os.environ.get("GROQ_API_KEY")
 
-SAMPLE_RATE = 44100
-DURATION = 5
-
-
-def record_audio(filename: str, duration: int, sample_rate: int):
-    print("Recording started...")
-    
-    audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=2, dtype='int16')
-    sd.wait() 
-    print("Recording finished.")
-
-    with wave.open(filename, 'wb') as wf:
-        wf.setnchannels(2)
-        wf.setsampwidth(2)  
-        wf.setframerate(sample_rate)
-        wf.writeframes(audio_data.tobytes())
-
-
-def get_command(filename):
+async def get_command(filename):
     client = Groq()
     
     filename = os.path.dirname(__file__) + "/Audio/output_audio.wav"
@@ -59,15 +40,6 @@ def get_command(filename):
         stop=None,
         stream=False,
     )
-    
     vscode_command = chat_completion.choices[0].message.content
-    print("VSCODE Command:", vscode_command)
+    print("VSCODE Command:", vscode_command)  #delete
 
-
-def main():
-    filename = os.path.dirname(__file__) + "/Audio/output_audio.wav"
-    record_audio(filename, DURATION, SAMPLE_RATE)
-    get_command(filename)
-
-if __name__ == "__main__":
-    main()
