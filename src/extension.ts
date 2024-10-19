67761d0de0aba3a1cfa0bf96a5bcb2ff80e29f88
Akilan Babu <wertyyyy102@gmail.com>
2024-10-19 10:34:57 -0700

    cleanup organization

--- src/extension.ts
+++ src/extension.ts
@@ -1,106 +1,47 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+const { spawn } = require("child_process");
+import * as vscode from "vscode";
 
-import * as vscode from 'vscode';
-import { LanguageClient } from 'vscode-languageclient/node';
-import { registerLogger, traceError, traceLog, traceVerbose } from './common/log/logging';
-import {
-    checkVersion,
-    getInterpreterDetails,
-    initializePython,
-    onDidChangePythonInterpreter,
-    resolveInterpreter,
-} from './common/python';
-import { restartServer } from './common/server';
-import { checkIfConfigurationChanged, getInterpreterFromSetting } from './common/settings';
-import { loadServerDefaults } from './common/setup';
-import { getLSClientTraceLevel } from './common/utilities';
-import { createOutputChannel, onDidChangeConfiguration, registerCommand } from './common/vscodeapi';
+let recordingProcess: any = null;
+const pythonPath = "/home/ecs_032c/code/Verbalist/venv/bin/python3";
+const filePath = "/home/ecs_032c/code/Verbalist/audio.py";
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("verbalist.captureAudio", () => {
+      const cmd = pythonPath;
+      const args = [filePath];
+      console.log("good mroning");
 
-let lsClient: LanguageClient | undefined;
-export async function activate(context: vscode.ExtensionContext): Promise<void> {
-    // This is required to get server name and module. This should be
-    // the first thing that we do in this extension.
-    const serverInfo = loadServerDefaults();
-    const serverName = serverInfo.name;
-    const serverId = serverInfo.module;
+      recordingProcess = spawn(cmd, args);
+      recordingProcess.stdout.on("data", (data: any) => {
+        console.log(`stdout from python: ${data}`);
+      });
+      recordingProcess.stderr.on("data", (data: any) => {
+        console.log(`STDERR from python: ${data}`);
+      });
+      vscode.window.showInformationMessage("spawned recording process");
+      recordingProcess.stdin.write("start" + "\n");
+      vscode.window.showInformationMessage("started recording process!");
 
-    // Setup logging
-    const outputChannel = createOutputChannel(serverName);
-    context.subscriptions.push(outputChannel, registerLogger(outputChannel));
+      return recordingProcess;
+    })
+  );
 
-    const changeLogLevel = async (c: vscode.LogLevel, g: vscode.LogLevel) => {
-        const level = getLSClientTraceLevel(c, g);
-        await lsClient?.setTrace(level);
-    };
-
-    context.subscriptions.push(
-        outputChannel.onDidChangeLogLevel(async (e) => {
-            await changeLogLevel(e, vscode.env.logLevel);
-        }),
-        vscode.env.onDidChangeLogLevel(async (e) => {
-            await changeLogLevel(outputChannel.logLevel, e);
-        }),
-    );
-
-    // Log Server information
-    traceLog(`Name: ${serverInfo.name}`);
-    traceLog(`Module: ${serverInfo.module}`);
-    traceVerbose(`Full Server Info: ${JSON.stringify(serverInfo)}`);
-
-    const runServer = async () => {
-        const interpreter = getInterpreterFromSetting(serverId);
-        if (interpreter && interpreter.length > 0) {
-            if (checkVersion(await resolveInterpreter(interpreter))) {
-                traceVerbose(`Using interpreter from ${serverInfo.module}.interpreter: ${interpreter.join(' ')}`);
-                lsClient = await restartServer(serverId, serverName, outputChannel, lsClient);
-            }
-            return;
-        }
-
-        const interpreterDetails = await getInterpreterDetails();
-        if (interpreterDetails.path) {
-            traceVerbose(`Using interpreter from Python extension: ${interpreterDetails.path.join(' ')}`);
-            lsClient = await restartServer(serverId, serverName, outputChannel, lsClient);
-            return;
-        }
-
-        traceError(
-            'Python interpreter missing:\r\n' +
-                '[Option 1] Select python interpreter using the ms-python.python.\r\n' +
-                `[Option 2] Set an interpreter using "${serverId}.interpreter" setting.\r\n` +
-                'Please use Python 3.8 or greater.',
-        );
-    };
-
-    context.subscriptions.push(
-        onDidChangePythonInterpreter(async () => {
-            await runServer();
-        }),
-        onDidChangeConfiguration(async (e: vscode.ConfigurationChangeEvent) => {
-            if (checkIfConfigurationChanged(e, serverId)) {
-                await runServer();
-            }
-        }),
-        registerCommand(`${serverId}.restart`, async () => {
-            await runServer();
-        }),
-    );
-
-    setImmediate(async () => {
-        const interpreter = getInterpreterFromSetting(serverId);
-        if (interpreter === undefined || interpreter.length === 0) {
-            traceLog(`Python extension loading`);
-            await initializePython(context.subscriptions);
-            traceLog(`Python extension loaded`);
-        } else {
-            await runServer();
-        }
-    });
+  context.subscriptions.push(
+    vscode.commands.registerCommand("verbalist.stopAudio", () => {
+      if (recordingProcess) {
+        vscode.window.showInformationMessage("stopping recording process!");
+        recordingProcess.stdin.write("stop\n");
+        recordingProcess.kill("SIGINT");
+        recordingProcess.on("exit", () => {
+          recordingProcess = null;
+        });
+      } else {
+        vscode.window.showInformationMessage("no recording process to kill");
+      }
+    })
+  );
+  //   context.subscriptions.push(disposable);
 }
 
-export async function deactivate(): Promise<void> {
-    if (lsClient) {
-        await lsClient.stop();
-    }
-}
+// This method is called when your extension is deactivated
+export function deactivate() {}
